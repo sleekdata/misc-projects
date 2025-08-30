f56562f02dbc3cb09c52fa41b288481a4a6c2785
sleekdata <143328011+sleekdata@users.noreply.github.com>
2025-08-31 01:14:49 +1200

    Update airflow-start.sh

--- airflow-setup/airflow-start.sh
+++ airflow-setup/airflow-start.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Before running, update the AIRFLOW_VERSION below as needed
-AIRFLOW_VERSION=3.0.4
+AIRFLOW_VERSION=3.0.6
 
 # Activate the virtual environment
 source ~/venvs/airflow_${AIRFLOW_VERSION}/bin/activate
